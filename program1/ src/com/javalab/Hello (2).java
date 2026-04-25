fda4ecd927bc92712b923a8df2f04c336cb07b0e
stephenstephenkbd-source <stephenstephenkbd@gmail.com>
2026-04-25 10:59:40 +0530

    Add files via upload

---
+++ program1/ src/com/javalab/Hello (2).java
@@ -0,0 +1,14 @@
+package com.scsvmv.javalab;
+
+/*
+ *simple Hello class
+ *Always wishes Hello World
+ */
+
+public class Hello
+{
+  public void wish()
+  {
+    System.out.println("Hello World");
+  }
+}
