f6d4cb3a813776664daf92263d3005285e51d0d0
stephenstephenkbd-source <stephenstephenkbd@gmail.com>
2026-04-25 10:48:14 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,35 @@
+package com.scsvmv.javalab.hello;
+
+import java.util.Date;
+
+/*
+ *Hello class
+ *Demonstrates method overloading and command-line arguments
+ */
+
+public class Hello
+{
+   //Default constructor
+     public Hello()
+     {
+     }
+
+   //Generic wish
+     public void wish()
+     {
+       System.out.println("Hello World");
+     }
+
+   //Overloaded wish method with name parameter 
+     public void wish(String name)
+     {
+       System.out.println("Hello"+name);
+     }
+
+   //wish with name and today's date
+     public void wishWithDate(String name)
+     {
+       Date today=new Date();
+       System.out.println("Hello"+name+"|Date:"+today);
+     }
+} 
